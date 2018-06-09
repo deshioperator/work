92c2bd5c202562b419f990779e85b4a1d8476d82
Sufian Khan <dgitalvoice@gmail.com>
2018-06-09 23:21:07 +0600

    Add files via upload

---
+++ default-javascript.js
@@ -0,0 +1,101 @@
+$('.menu-btn').on('click', function () {
+
+		var elem = $(this),
+		    item = $('.menu__item'),
+		    active = 'is-active',
+		    play = 'menu__item--play';
+
+		if (  elem.hasClass(active) ) {
+			elem.removeClass(active);
+			$(item.get().reverse()).each(function(i) {
+				var row = $(this);
+					setTimeout(function() {
+						row.removeClass(play);
+				}, 50*i);
+			});
+		}
+
+		else {
+			elem.addClass(active);
+			item.each(function(i) {
+				var row = $(this);
+					setTimeout(function() {
+						row.addClass(play);
+				}, 50*i);
+			});
+		}
+
+	});
+
+(function($) {
+
+  $.fn.menumaker = function(options) {
+      
+      var cssmenu = $(this), settings = $.extend({
+        title: "Menu",
+        format: "dropdown",
+        sticky: false
+      }, options);
+
+      return this.each(function() {
+        cssmenu.prepend('<div id="menu-button">' + settings.title + '</div>');
+        $(this).find("#menu-button").on('click', function(){
+          $(this).toggleClass('menu-opened');
+          var mainmenu = $(this).next('ul');
+          if (mainmenu.hasClass('open')) { 
+            mainmenu.hide().removeClass('open');
+          }
+          else {
+            mainmenu.show().addClass('open');
+            if (settings.format === "dropdown") {
+              mainmenu.find('ul').show();
+            }
+          }
+        });
+
+        cssmenu.find('li ul').parent().addClass('has-sub');
+
+        multiTg = function() {
+          cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+          cssmenu.find('.submenu-button').on('click', function() {
+            $(this).toggleClass('submenu-opened');
+            if ($(this).siblings('ul').hasClass('open')) {
+              $(this).siblings('ul').removeClass('open').hide();
+            }
+            else {
+              $(this).siblings('ul').addClass('open').show();
+            }
+          });
+        };
+
+        if (settings.format === 'multitoggle') multiTg();
+        else cssmenu.addClass('dropdown');
+
+        if (settings.sticky === true) cssmenu.css('position', 'fixed');
+
+        resizeFix = function() {
+          if ($( window ).width() > 768) {
+            cssmenu.find('ul').show();
+          }
+
+          if ($(window).width() <= 768) {
+            cssmenu.find('ul').hide().removeClass('open');
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+
+      });
+  };
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+
+$("#cat-menu").menumaker({
+   title: "Menu",
+   format: "multitoggle"
+});
+
+});
+})(jQuery);
